b38a5fdb05d8999554e540e97961e889d4398688
BrMohammed <togachivon@gmail.com>
2024-12-08 19:40:53 +0100

    end card animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum Levels
 {
@@ -11,6 +13,7 @@ public enum Levels
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject winobj;
     public float distence;
     public GameObject obj;
     Camera cam;
@@ -29,6 +32,8 @@ public class GameManager : MonoBehaviour
     private float rotationX = 0f;
     private float rotationY = 0f;
 
+    bool win = false;
+
     void Start()
     {
         cam = Camera.main;
@@ -37,66 +42,81 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        bool isCtrlDown = Input.GetKey(KeyCode.LeftControl);
-        bool isShiftDown = Input.GetKey(KeyCode.LeftShift);
-        bool isMouse0Down = Input.GetKey(KeyCode.Mouse0);
-        bool isMouse0Up = Input.GetMouseButtonUp(0);
-        Ray ray = cam.ScreenPointToRay(mousePos);
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        if(!win)
         {
-            if (isMouse0Down)
+            Vector3 mousePos = Input.mousePosition;
+            bool isCtrlDown = Input.GetKey(KeyCode.LeftControl);
+            bool isShiftDown = Input.GetKey(KeyCode.LeftShift);
+            bool isMouse0Down = Input.GetKey(KeyCode.Mouse0);
+            bool isMouse0Up = Input.GetMouseButtonUp(0);
+            Ray ray = cam.ScreenPointToRay(mousePos);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
-               isDragging = true;
+                if (isMouse0Down)
+                {
+                    isDragging = true;
+                }
+
+
             }
 
-           
-        }
+            if (isMouse0Up)
+            {
+                isDragging = false;
+            }
 
-        if (isMouse0Up)
-        {
-            isDragging = false;
-        }
+            //if (isDragging && !isCtrlDown && !isShiftDown)
+            //{
+            //    float deltaX = tempMousePos.x - mousePos.x;
+            //    obj.transform.Rotate(Vector3.up * deltaX * speed * Time.deltaTime);
+            //}
+            //if (isDragging && isCtrlDown && !isShiftDown)
+            //{
+            //    float deltaX = tempMousePos.y - mousePos.y;
+            //    obj.transform.Rotate(Vector3.right * deltaX * speed * Time.deltaTime);
+            //}
+            if (isDragging && !isCtrlDown && !isShiftDown)
+            {
+                float deltaX = tempMousePos.x - mousePos.x;
+                obj.transform.Rotate(0, deltaX * speed * Time.deltaTime, 0, Space.World);
+            }
 
-        //if (isDragging && !isCtrlDown && !isShiftDown)
-        //{
-        //    float deltaX = tempMousePos.x - mousePos.x;
-        //    obj.transform.Rotate(Vector3.up * deltaX * speed * Time.deltaTime);
-        //}
-        //if (isDragging && isCtrlDown && !isShiftDown)
-        //{
-        //    float deltaX = tempMousePos.y - mousePos.y;
-        //    obj.transform.Rotate(Vector3.right * deltaX * speed * Time.deltaTime);
-        //}
-        if (isDragging && !isCtrlDown && !isShiftDown )
-        {
-            float deltaX = tempMousePos.x - mousePos.x;
-            obj.transform.Rotate(0, deltaX * speed * Time.deltaTime, 0, Space.World);
-        }
+            if (isDragging && isCtrlDown && !isShiftDown && (levels == Levels.vertical || levels == Levels.movement))
+            {
+                float deltaY = mousePos.y - tempMousePos.y;
+                obj.transform.Rotate(deltaY * speed * Time.deltaTime, 0, 0, Space.World);
+            }
+            if (isDragging && !isCtrlDown && isShiftDown && levels == Levels.movement)
+            {
+                Vector3 worldMousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, distence));
+                obj.transform.position = new Vector3(worldMousePos.x, worldMousePos.y, 0);
 
-        if (isDragging && isCtrlDown && !isShiftDown && (levels == Levels.vertical || levels == Levels.movement))
-        {
-            float deltaY =  mousePos.y - tempMousePos.y;
-            obj.transform.Rotate(deltaY * speed * Time.deltaTime, 0, 0, Space.World);
+            }
+            if (IsRotationCloseToTarget(obj.transform.eulerAngles, rotate, tolerance))
+            {
+                Debug.Log("Rotation is close to the target!");
+                obj.transform.eulerAngles = rotate;
+                win = true;
+                winobj.SetActive(true);
+                int sceneName = int.Parse(SceneManager.GetActiveScene().name) ;
+                if(sceneName > MainManager.init.Getint("Level" ))
+                     MainManager.init.SetInt("Level", sceneName);
+            }
+            tempMousePos = mousePos;
         }
-        if (isDragging && !isCtrlDown && isShiftDown && levels == Levels.movement)
-        {
-            Vector3 worldMousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, distence));
-            obj.transform.position = new Vector3(worldMousePos.x, worldMousePos.y,0);
-
-        }
-        if (IsRotationCloseToTarget(obj.transform.eulerAngles, rotate, tolerance))
-        {
-            Debug.Log("Rotation is close to the target!");
-        }
-        tempMousePos = mousePos;
+      
     }
     bool IsRotationCloseToTarget(Vector3 currentRotation, Vector3 targetRotation, float tolerance)
     {
         return Mathf.Abs(currentRotation.x - targetRotation.x) <= tolerance &&
                Mathf.Abs(currentRotation.y - targetRotation.y) <= tolerance &&
                Mathf.Abs(currentRotation.z - targetRotation.z) <= tolerance;
+    }
+
+    public void ChengeScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,15 +1,161 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
+    [SerializeField] private int numberofscens = 5;
     [SerializeField] private GameObject cam;
+    [SerializeField] private GameObject Levelcam;
     [SerializeField] private GameObject startbtn;
+    [SerializeField] private float timeoftransactioncofcam = 2;
+    [SerializeField] private float TimeofMoveCardes = 2;
+    [SerializeField] private float floatHeightForcard = 0.5f;
+    public LayerMask layerMask;
+    [SerializeField] private Vector3 OfssetOfCamMovement;
+    Camera _cam;
+    [SerializeField] private GameObject _fadeui;
+    public GameObject[] AllCards;
+    public GameObject[] CardsPosetion;
+
+    
+
+    static public MainManager init;
+    bool animation = false;
+
+    private void Awake()
+    {
+        if (init == null)
+            init = this;
+    }
+    private void Start()
+    {
+        print("Animated :" + Getint("Animated") + "Level :" + Getint("Level"));
+        _cam = Camera.main;
+        int Animated = Getint("Animated");
+        int Level = Getint("Level");
+        if (Animated != 0)
+        {
+            for (int i = 0; i < Animated; i++)
+            {
+                AllCards[i].transform.position = CardsPosetion[i].transform.position;
+            }
+            for (int i = 0; i < Animated - 1; i++)
+            {
+                AllCards[i].transform.rotation = Quaternion.Euler(270, 180, 0);
+            }
+            if(Level == numberofscens)
+            {
+                AllCards[Level - 1].transform.rotation = Quaternion.Euler(270, 180, 0);
+            }
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if(gameObject.activeSelf && !animation)
+        {
+            int level = Getint("Level");
+            int Animated = Getint("Animated");
+            if (AllCards.Length > 0 && CardsPosetion.Length > 0)
+            {
+               
+                if (Animated == level && level < numberofscens)
+                {
+                    if(level != 0 && level != numberofscens)
+                        AllCards[level - 1].GetComponent<Animator>().enabled = !AllCards[level - 1].GetComponent<Animator>().enabled;
+                    StartCoroutine(Flipcard());
+                }
+               
+                
+            }
+            animation = true;
+        }
+            
+            Vector3 mousePos = Input.mousePosition;
+            bool isMouse0Up = Input.GetMouseButtonUp(0);
+            Ray ray = Levelcam.GetComponent<Camera>().ScreenPointToRay(mousePos);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+
+                if (isMouse0Up)
+                {
+                    Vector3 targetPosition = hit.transform.position + OfssetOfCamMovement;
+                    _fadeui.GetComponent<Animator>().enabled = !_fadeui.GetComponent<Animator>().enabled;
+                    LeanTween.move(Levelcam, targetPosition, timeoftransactioncofcam).setOnComplete(() =>
+                    {
+                        SceneManager.LoadScene(hit.transform.gameObject.name);
+                    });
+                }
+            }
+     
+        
+    }
+
+    IEnumerator Flipcard()
+    {
+        yield return new WaitForSeconds(2);
+        int level = Getint("Level");
+        MoveCardWithCurveEffect(AllCards[level], CardsPosetion[level].transform.position);
+    }
+
+
+    void MoveCardWithCurveEffect(GameObject card, Vector3 targetPosition)
+    {
+
+
+        Vector3 startPoint = card.transform.position;
+        Vector3 midPoint = Vector3.Lerp(startPoint, targetPosition, 0.5f);
+        midPoint.y += floatHeightForcard;
+        Vector3 endPoint = targetPosition;
+        Vector3[] path = new Vector3[] { startPoint, startPoint, midPoint, endPoint, endPoint };
+        LeanTween.moveSpline(card, path, TimeofMoveCardes).setEase(LeanTweenType.easeOutQuad).setOnComplete(()=>
+        {
+            int level = Getint("Level");
+            int animated = Getint("Animated");
+            if(level == animated)
+            {
+                //card.GetComponent<Animator>().enabled = !card.GetComponent<Animator>().enabled;
+                SetInt("Animated",level + 1);
+            }
+        });
+    }
+    public void SetInt(string KeyName, int Value)
+    {
+        PlayerPrefs.SetInt(KeyName, Value);
+    }
+
+    public int Getint(string KeyName)
+    {
+        return PlayerPrefs.GetInt(KeyName,0);
+    }
+
+
+
 
     public void GoToMenue()
     {
         cam.GetComponent<Animator>().enabled = !cam.GetComponent<Animator>().enabled;
         startbtn.SetActive(false);
+        StartCoroutine(EnableLevelCam());
+    }
+
+
+    public void Resetlevels()
+    {
+        SetInt("Animated", 0 );
+        SetInt("Level", 0 );
+    }
+    IEnumerator EnableLevelCam()
+    {
+        yield return new WaitForSeconds(3);
+        Levelcam.SetActive(true);
+        cam.SetActive(false);
     }
 }
